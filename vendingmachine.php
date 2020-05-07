6de7f8c1487f0f4cf920090a2a3f47aa35317854
Iñaki Iglesias <inaki@nukstudio.com>
2020-05-07 19:13:34 +0200

    Created autoloader and basic php program for console

---
+++ vendingmachine.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+
+require 'autoloader.php';
+
+$shell = new \UI\Console\Reader();
+echo 'Starting Vending Machine Service...'.PHP_EOL;
+
+while ($shell->readLine()) {
+	if (!$shell->quitted()) {
+		echo $shell->executeCommand().PHP_EOL;
+	} else {
+		break;
+	}
+}
+
+echo 'Quitting Vending Machine service...'.PHP_EOL;
